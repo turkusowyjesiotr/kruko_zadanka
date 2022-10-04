1d08f9980492f3b664bb0915b23b96a6bf3647ce
turkusowyjesiotr <patrykpilcicki+github@gmail.com>
2022-10-04 08:44:47 +0200

    fixed typos

--- Classes/find_the_torsional_angle.py
+++ Classes/find_the_torsional_angle.py
@@ -4,16 +4,27 @@ import math
 
 class Points(object):
     def __init__(self, x, y, z):
-        self.x, self.y, self.z = x, y, z
+        self.x = x
+        self.y = y
+        self.z = z
 
     def __sub__(self, no):
-        return Points(self. x - no.x, self. y - no.y, self. z - no.z)
+        x = self.x - no.x
+        y = self.y - no.y
+        z = self.z - no.z
+        return Points(x, y, z)
 
     def dot(self, no):
-        return self. x * no.x + self. y * no.y + self. z * no.z
+        x = self.x * no.x
+        y = self.y * no.y
+        z = self.z * no.z
+        return x + y + z
 
     def cross(self, no):
-        return Points(self. y * no.z - self. z * no.y, self. z * no.x - self. x * no.z, self. x * no.y - self. y * no.x)
+        x = self.y * no.z - self.z * no.y
+        y = self.z * no.x - self.x * no.z
+        z = self.x * no.y - self.y * no.x
+        return Points(x, y, z)
 
     def absolute(self):
         return pow((self.x ** 2 + self.y ** 2 + self.z ** 2), 0.5)
